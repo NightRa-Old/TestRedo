b1749b2eeed4127e53540a6f9e81e7234203092d
NightRa <ilan3580@gmail.com>
2013-02-23 16:08:03 +0200

    Update test/DateConverterTest.java

--- test/DateConverterTest.java
+++ test/DateConverterTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class DateConverterTest {
+    //Please note that because I'm forced to put everything into one method, I can't really test much.
+    
     @Test
     public void testConvertToUSA() throws Exception {
         String date = "13/06/2013";
